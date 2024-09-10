0a0d668df303cfe9961d791d75bd5117d3714b83
Vitor <vitorsantabi@gmail.com>
2024-09-10 15:27:18 -0300

    fundamentos 001

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+function start() {
+
+    while (true){
+        let opcao = "sair"
+        switch (opcao) {
+            case "cadastrar":
+                console.log("Cadastrando")
+                break
+            case "listar":
+                console.log("Listando")
+                break
+            case "sair":
+                console.log("Saindo")
+                return
+        }
+    }
+}
+start()
